0efaa443dec29e2433385b2ba2cd8f81da0dbcac
gorhoz <gio.gvetadze@gmail.com>
2023-11-14 19:12:58 +0400

    added input exercise

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Books from './components/Books'
+import Input from './components/Input'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-        <Books />
+        {/* <Books /> */}
+        <Input />
       
     </div>
   )
---
+++ src/components/Input.jsx
@@ -0,0 +1,19 @@
+
+
+function Input  () {
+const handleChange = (value) => {
+    value.length>10?alert("Maximum number of symbols is 10"):""
+}
+
+    return (
+        <div>
+            <div>
+                <input type="text" placeholder="name" onChange={(e)=> handleChange(e.target.value)}>
+                    
+                </input>
+            </div>
+        </div>
+    )
+}
+
+export default Input;
